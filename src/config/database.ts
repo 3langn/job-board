985801f087dd5b1ceba8c50ad317aad8f5cea1df
Minh Nguyen <3langn@gmail.com>
2022-03-23 19:20:11 +0700

    My first commit

--- src/config/database.ts
+++ src/config/database.ts
@@ -7,7 +7,8 @@ export const config = () => ({
   },
   database: {
     type: 'postgres',
-    host: process.env.DB_HOST,
+    // host: process.env.DB_HOST,
+    // port: process.env.DB_PORT,
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
