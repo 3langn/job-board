52f05f8113277e59192c8aaeaaa7c18f2eafed42
Minh Nguyen <3langn@gmail.com>
2022-03-24 02:13:34 +0700

    My first commit

--- src/config/database.ts
+++ src/config/database.ts
@@ -2,12 +2,12 @@ export const config = () => ({
   port: process.env.PORT,
   database: {
     type: 'postgres',
-    url: process.env.ENV !== 'dev' ? process.env.DATABASE_URL : null,
+    url: process.env.ENV === 'prod' ? process.env.DATABASE_URL : null,
     ssl: {
       rejectUnauthorized: false,
     },
-    host: process.env.ENV === 'dev' ? process.env.DB_HOST : null,
-    port: process.env.ENV === 'dev' ? process.env.DB_PORT : null,
+    host: process.env.ENV !== 'prod' ? process.env.DB_HOST : null,
+    port: process.env.ENV !== 'prod' ? process.env.DB_PORT : null,
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
