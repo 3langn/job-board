e6b1c7a5772fa77ba70e4ac9d8ddb014e3a89ead
Minh Nguyen <3langn@gmail.com>
2022-03-25 16:12:25 +0700

    init

--- src/upload/upload.controller.ts
+++ src/upload/upload.controller.ts
@@ -1,4 +1,27 @@
-import { Controller } from '@nestjs/common';
+import { Controller, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { ApiBody, ApiConsumes } from '@nestjs/swagger';
+import { UploadService } from './upload.service';
 
 @Controller('upload')
-export class UploadController {}
+export class UploadController {
+  constructor(private readonly uploadService: UploadService) {}
+
+  @ApiConsumes('multipart/form-data')
+  @ApiBody({
+    schema: {
+      type: 'object',
+      properties: {
+        file: {
+          type: 'string',
+          format: 'binary',
+        },
+      },
+    },
+  })
+  @UseInterceptors(FileInterceptor('file'))
+  async uploadFile(@UploadedFile() file: Express.Multer.File) {
+    const fileUploaded = await this.uploadService.uploadStream(file);
+    return fileUploaded.secure_url;
+  }
+}
