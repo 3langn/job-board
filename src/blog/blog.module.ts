47fdae6f3ac6bd62c7468e76b70940672e53dee3
Minh Nguyen <3langn@gmail.com>
2022-04-04 00:40:18 +0700

    fix

--- src/blog/blog.module.ts
+++ src/blog/blog.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { CompanyModule } from 'src/company/company.module';
 import { UserModule } from 'src/user/user.module';
 import { BlogEntity } from './blog';
 import { BlogController } from './blog.controller';
 import { BlogService } from './blog.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([BlogEntity]), UserModule],
+  imports: [TypeOrmModule.forFeature([BlogEntity]), UserModule, CompanyModule],
   controllers: [BlogController],
   providers: [BlogService],
 })
